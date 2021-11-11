7149b39a9f973e6ec3af15753c859fe5217713b4
Jani Suoranta <janisuoranta@icloud.com>
2021-11-11 09:25:54 +0200

    osa3 3.1

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 const express = require("express")
 const app = express()
 
+app.use(express.json())
+
 let persons =
 	[
 		{
-			"name": "Arto Hellas",
-			"number": "040-124512",
-			"id": 1
+			name: "Arto Hellas",
+			number: "040-124512",
+			id: 1
 		},
 		{
-			"name": "Ada Lovelace",
-			"number": "23-42-3241214834",
-			"id": 2
+			name: "Ada Lovelace",
+			number: "23-42-3241214834",
+			id: 2
 		},
 		{
-			"name": "Dominic Khan",
-			"number": "39-44-125123",
-			"id": 3
+			name: "Dominic Khan",
+			number: "39-44-125123",
+			id: 3
 		},
 		{
-			"name": "Mary Poppendieck",
-			"number": "39-23-6423122",
-			"id": 4
+			name: "Mary Poppendieck",
+			number: "39-23-6423122",
+			id: 4
 		}
 	]
+
+app.get("/", (req, res) => {
+	res.send("<h1>Puhelinluettelo backend</h1>")
+})
+
+app.get("/api/persons", (req, res) => {
+	res.json(persons)
+})
 
 const PORT = 3001
 app.listen(PORT)
